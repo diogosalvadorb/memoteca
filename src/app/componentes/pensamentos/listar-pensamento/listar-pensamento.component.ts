a5a90b5be01feb78f173aadead5c51f0e6aaab82
diogosalvadorb <contact.diogosalvador@gmail.com>
2023-04-03 16:34:26 -0300

    http client service Pensamento

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -9,9 +10,9 @@ import { Pensamento } from '../pensamento';
 export class ListarPensamentoComponent {
   listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   NgOnInit(): void {
-
+    this.service.listar()
   }
 }
